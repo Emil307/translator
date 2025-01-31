76ad5eb7213a699fc2bd0b34eac5aa7243895db2
Emil307 <emilnovikov392@gmail.com>
2025-01-31 12:56:40 +0600

    chore: layout header

--- src/views/translator/ui/TranslatorScreen.tsx
+++ src/views/translator/ui/TranslatorScreen.tsx
@@ -2,10 +2,12 @@ import { View } from "react-native";
 import { Translator } from "@/src/widgets/translator";
 import { styles } from "../styles";
 import { History } from "@/src/widgets/history";
+import { Header } from "@/src/widgets/header";
 
 export default function TranslatorScreen() {
   return (
     <View style={styles.container}>
+      <Header />
       <Translator />
       <History />
     </View>
---
+++ src/widgets/header/styles/index.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: 700,
+    fontSize: 16,
+    lineHeight: 16,
+    letterSpacing: 1,
+    textTransform: "uppercase",
+    color: "#fff",
+  },
+});
