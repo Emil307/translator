3bb91a803cdd94466401d4cfdd4c2c49e1162ad2
Emil307 <emilnovikov392@gmail.com>
2025-01-31 21:28:22 +0600

    feat: record audio in file

--- src/widgets/translator/ui/Translator.tsx
+++ src/widgets/translator/ui/Translator.tsx
@@ -2,10 +2,56 @@ import React, { useState } from "react";
 import { styles } from "../styles";
 import { View, Image, Text, TextInput, TouchableOpacity } from "react-native";
 import * as Clipboard from "expo-clipboard";
+import { Audio } from "expo-av";
 
 export const Translator: React.FC = () => {
   const [initialText, setInitialText] = useState("");
   const [isRecording, setIsRecording] = useState(false);
+  const [audioUri, setAudioUri] = useState<string | null>(null);
+  const [recording, setRecording] = useState<Audio.Recording | null>(null);
+
+  const startRecording = async () => {
+    try {
+      // Устанавливаем режим аудио для записи
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true, // Разрешаем запись на iOS
+        playsInSilentModeIOS: true, // Разрешаем воспроизведение в тихом режиме на iOS
+      });
+
+      // Запрашиваем разрешение на использование микрофона
+      const { status } = await Audio.requestPermissionsAsync();
+      if (status !== "granted") {
+        alert("Разрешение на запись аудио не получено!");
+        return;
+      }
+
+      // Создаем новый объект для записи аудио
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      setIsRecording(true);
+
+      // Начинаем запись
+      await recording.startAsync();
+    } catch (error) {
+      console.error("Ошибка при записи:", error);
+    }
+  };
+
+  const stopRecording = async () => {
+    try {
+      if (recording) {
+        await recording.stopAndUnloadAsync();
+        const uri = recording.getURI();
+        setAudioUri(uri);
+        setIsRecording(false);
+        setRecording(null);
+      }
+    } catch (error) {
+      console.error("Ошибка при остановке записи:", error);
+    }
+  };
 
   return (
     <View style={styles.screen}>
@@ -36,7 +82,7 @@ export const Translator: React.FC = () => {
               />
             </TouchableOpacity>
             {!isRecording && (
-              <TouchableOpacity onPress={() => setIsRecording(true)}>
+              <TouchableOpacity onPress={startRecording}>
                 <Image
                   style={styles.recordImage}
                   source={require("@/assets/images/record.png")}
@@ -45,7 +91,7 @@ export const Translator: React.FC = () => {
             )}
             {isRecording && (
               <TouchableOpacity
-                onPress={() => setIsRecording(false)}
+                onPress={stopRecording}
                 style={styles.recordingButton}
               ></TouchableOpacity>
             )}
