01ce46cf0b15d6e5d69a4e95919d74aa0fa835bf
Emil307 <emilnovikov392@gmail.com>
2025-02-02 21:39:01 +0600

    chore: redirect by auth

--- src/application/layouts/index.tsx
+++ src/application/layouts/index.tsx
@@ -1,5 +1,15 @@
-import { Slot } from "expo-router";
+import { router, Slot } from "expo-router";
+import { useEffect } from "react";
 
 export function RootLayout() {
+  const isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      router.push("/auth/signin");
+      return;
+    }
+  }, []);
+
   return <Slot />;
 }
